0f033270d48d6089261aaf0940c698715015a37e
BryanHuerta <bhuertar@DESKTOP-OUBEMJ8>
2024-04-22 14:32:22 -0500

    changed tables func

---
+++ code.js
@@ -0,0 +1,52 @@
+// HERE I WILL PUT ANY CODE TO THE SIDE THAT MIGHT BE USEFULL
+
+// const createUser = async() => {
+//   const userOne = await prisma.user.create({
+//     data: {
+//       username: faker.internet.userName(),
+//       password: faker.internet.password()
+//     }
+//   })
+
+//   const userTwo = await prisma.user.create({
+//     data: {
+//       username: faker.internet.userName(),
+//       password: faker.internet.password()
+//     }
+//   })
+
+//   const userThree = await prisma.user.create({
+//     data: {
+//       username: faker.internet.userName(),
+//       password: faker.internet.password()
+//     }
+//   })
+// }
+
+
+// const createRobot = async() => {
+//   const robotOne = await prisma.robot.create({
+//     data: {
+//       name: faker.vehicle.manufacturer(),
+//       color: faker.color.human(),
+//       userId: 1
+//     }
+//   })
+
+//   const robotTwo = await prisma.robot.create({
+//     data: {
+//       name: faker.vehicle.manufacturer(),
+//       color: faker.color.human(),
+//       userId: 2
+//     }
+//   })
+
+//   const robotThree = await prisma.robot.create({
+//     data: {
+//       name: faker.vehicle.manufacturer(),
+//       color: faker.color.human(),
+//       userId: 3
+//     }
+//   })
+
+// }
--- db/seed.js
+++ db/seed.js
@@ -6,52 +6,26 @@ const prisma = new PrismaClient();
 const { faker } = require('@faker-js/faker');
 
 const createUser = async() => {
-  const userOne = await prisma.user.create({
-    data: {
-      username: faker.internet.userName(),
-      password: faker.internet.password()
-    }
-  })
-
-  const userTwo = await prisma.user.create({
-    data: {
-      username: faker.internet.userName(),
-      password: faker.internet.password()
-    }
-  })
-
-  const userThree = await prisma.user.create({
-    data: {
-      username: faker.internet.userName(),
-      password: faker.internet.password()
-    }
-  })
+  for(let i = 0; i < 3; i++) {
+    await prisma.user.create({
+      data: {
+        username: faker.internet.userName(),
+        password: faker.internet.password()
+      }
+    })
+  }
 }
 
 const createRobot = async() => {
-  const robotOne = await prisma.robot.create({
-    data: {
-      name: faker.vehicle.manufacturer(),
-      color: faker.color.human(),
-      userId: 1
-    }
-  })
-
-  const robotTwo = await prisma.robot.create({
-    data: {
-      name: faker.vehicle.manufacturer(),
-      color: faker.color.human(),
-      userId: 2
-    }
-  })
-
-  const robotThree = await prisma.robot.create({
-    data: {
-      name: faker.vehicle.manufacturer(),
-      color: faker.color.human(),
-      userId: 3
-    }
-  })
+  for(let i = 0; i < 9; i++) {
+    await prisma.robot.create({
+      data: {
+        name: faker.vehicle.manufacturer(),
+        color: faker.color.human(),
+        userId: (i % 3) + 1
+      }
+    })
+  }
 
 }
 
